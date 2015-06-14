8e89b79b7559a9d1fdd23265ac266a6f8ee31d68
stevejb1957@hotmail.com <stevejb1957@hotmail.com>
2015-06-14 15:14:16 +0100

    Replace name Prompt with canvas equivalent

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -1,8 +1,11 @@
-     var my_pic = new Image();
+var input;
+
+var my_pic = new Image();
      my_pic.src = "../Images/horse.jpg";
      var h = 413; 
 
-function title(){
+function title()
+{
     
     var ctx = document.getElementById('my_canvas').getContext('2d'); 
         // fillText(text,x,y,maxWidth)
@@ -16,9 +19,17 @@ function title(){
         ctx.fillText("Guess the Animal Game", 130,100);
         ctx.strokeText("Guess the Animal Game",130,100);
         
-    name = prompt("What is your name");    
-        
-    }
+      
+     
+    input = new CanvasInput 
+    ({ 
+        canvas: document.getElementById('my_canvas'),
+        placeHolder: "What is your name?",
+        onsubmit: begin
+    });
+}
+
+
 
 //function slidePic(){
     
@@ -65,23 +76,27 @@ var ctx = document.getElementById('my_canvas').getContext('2d');
         ctx.shadowBlur = 2;
         ctx.font = "bold 50px Comic Sans MS";
         ctx.fillText("", 130,100);
-        ctx.fillText("Hello " + name + " let's play a game", 250,300);
-        ctx.strokeText("Hello " + name + " let's play a game",250,300);
+        ctx.fillText("Hello " + input.value() + " let's play a game", 250,300);
+        ctx.strokeText("Hello " + input.value() + " let's play a game",250,300);
         ctx.fillStyle = "rgba(77, 217, 59, 0.72)";
         ctx.fillText("What am I?",450,400)
 }
 
+function begin()
+{
 
+    window.setTimeout(function(){
+     play();
+    }, 1500);
 
-window.setTimeout(function(){
- play();
-}, 1500);
- 
-window.setTimeout(function(){
- animal();
-},2500);
+    window.setTimeout(function(){
+     animal();
+    },2500);
+        
+}
     
-    function animal(){
+function animal(){
+    
        
      var ctx = document.getElementById('my_canvas').getContext('2d');
         // ctx.drawimage(my_pic,clipX,clipY,clipW,clipH,x,y,w,h);
