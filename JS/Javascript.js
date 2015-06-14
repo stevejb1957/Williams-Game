fd2840f57005d55a72f062ba8c6d425621fadebd
stevejb1957@hotmail.com <stevejb1957@hotmail.com>
2015-06-14 17:19:34 +0100

    Tidy-up and dummy answer success state

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -1,10 +1,11 @@
 var input;
-
 var my_pic = new Image();
-     my_pic.src = "../Images/horse.jpg";
-     var h = 413; 
+my_pic.src = "../Images/horse.jpg";
+var h = 413; 
 var ctx;
 var canvas;
+
+window.onload=title
 
 function title()
 {
@@ -39,45 +40,8 @@ function title()
 }
 
 
-
-//function slidePic(){
-    
-    //alert("help");
-    //i=h
-   //if(i;i>1;i--){
-       //var ctx = document.getElementById('my_canvas').getContext('2d');
-     //ctx.drawImage(my_pic,0,0,620,413,325,450,620,i);
-   
-   //}
-    
-          
-//}
-
-window.onload=title
-
-
-
-
-//function prompt(){
- //alert("Hello");   
-//name = prompt("What is your name");
-
-//}
-
-//window.setTimeout(function(){
-//prompt();
-
-//},1000);
-
-
-
-
-    
- 
-
-function play ()
-{
-    
+function play()
+{    
     var ctx = document.getElementById('my_canvas').getContext('2d');
     ctx.fillStyle = "rgba(16, 60, 245, 0.72)";
     ctx.shadowColor = "#000";
@@ -98,39 +62,33 @@ function begin()
     var hiddenInput = children[4];
     document.body.removeChild(hiddenInput);
     ctx.clearRect(475, 148, 350, 52);
-    //var otherCanvas = input.renderCanvas();
-    //otherCanvas.getContext('2d').clearRect(0,0, otherCanvas.width, otherCanvas.height);
     window.setTimeout(function(){
      play();
     }, 500);
 
     window.setTimeout(function(){
      animal();
-    },1000);
-        
+    },1000);        
 }
     
-function animal(){
-    
-       
-     var ctx = document.getElementById('my_canvas').getContext('2d');
-        // ctx.drawimage(my_pic,clipX,clipY,clipW,clipH,x,y,w,h);
-        ctx.drawImage(my_pic,0,0,620,413,325,450,620,h);
-        
-    
-    } 
-    
-//window.setTimeout(function(){
-//slidePic();
+function animal()
+{     
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    ctx.drawImage(my_pic,0,0,620,413,325,450,620,h);
+} 
 
-//},2000);
-
-
+function checkAnswer(event,ui)
+{
+    var answer = ui.item.value;
+    alert(answer);
+}
     
   
- $(function() {
-    var availableTags = [
-      "Horse",
+$(function() 
+{
+    var availableTags = 
+    [
+        "Horse",
         "House",
         "Hello",
         "Cat",
@@ -140,28 +98,9 @@ function animal(){
         "Doll",
         "Dam"
     ];
-    $( "#tags" ).autocomplete({
-      source: availableTags
+    $( "#tags" ).autocomplete
+    ({
+        source: availableTags,
+        select: checkAnswer
     });
-  });
-
-//var response = document.getElementById("tags").value;
-//alert(response);
-
-//if (response = ("Horse") {
-    //alert("Well done");
-    
-    //}else {
-    //alert("Try again");
-    //}
-
-
-
-//alert(response);
-
-
-
-
-
-
-    
+});
